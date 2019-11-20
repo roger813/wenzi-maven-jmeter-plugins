b70b0482c372e3962d3294debfe1e19eefd224c7
liwenhuan <liwenhuan@zhongan.com>
2019-11-20 13:49:53 +0800

    优化随机生成日期算法

--- src/main/java/pers/wenzi/jmeter/plugins/functions/WZRandomDate.java
+++ src/main/java/pers/wenzi/jmeter/plugins/functions/WZRandomDate.java
@@ -15,43 +15,47 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-public class WZRandomBirth extends AbstractFunction {
+public class WZRandomDate extends AbstractFunction {
 
     private static final List<String> desc = new LinkedList<>();
-    private static final String KEY = "__WZRandomBirth";
+    private static final String KEY = "__WZRandomDate";
 
-    private String strStart;
-    private String strEnd;
+    private String dateS;
+    private String dateE;
     private String format;
     private String varname;
 
     static {
-        desc.add("Start Date, format: yyyy-MM-dd (required)");
-        desc.add("End Date, format: yyyy-MM-dd. Current time is default (optional)");
-        desc.add("Format, format u want to show. yyyy-MM-dd id default (optional)");
+        desc.add("Start Date, format is yyyy-MM-dd");
+        desc.add("End Date, format is yyyy-MM-dd. Current Date is default (optional)");
+        desc.add("Format, the format u want to show. yyyy-MM-dd id default (optional)");
         desc.add("Name of variable in which to store the result (optional)");
     }
 
-    private String getRandomDate() {
+    private String getRandomDate() throws Exception {
         SimpleDateFormat parseFormat = new SimpleDateFormat("yyyy-MM-dd");
         SimpleDateFormat outputFormat = new SimpleDateFormat(format);
-        Date dateStart;
-        Date dateEnd;
-        String date = null;
-        try {
-            dateStart = parseFormat.parse(strStart);
-            dateEnd = parseFormat.parse(strEnd);
-            long diff = (dateEnd.getTime() - dateStart.getTime());
-            long rand = 1000 + ((long) (new Random().nextDouble() * (diff - 1000)));
-            date = outputFormat.format(dateStart.getTime() + rand);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return date;
+        Date date1, date2;
+        date1 = parseFormat.parse(dateS);
+        date2 = parseFormat.parse(dateE);
+        long diff = (date2.getTime() - date1.getTime());
+        long rand = 1000 + ((long) (new Random().nextDouble() * (diff - 1000)));
+        return outputFormat.format(date1.getTime() + rand);
     }
 
     public String execute(SampleResult sampleResult, Sampler sampler) {
-        String result = getRandomDate();
+        String result;
+        if (dateS == null || dateS.length() < 1) {
+            System.out.println("Start Date is required, and text cannot be empty");
+            return null;
+        }
+        try {
+            result = getRandomDate();
+        } catch (Exception e) {
+            System.out.println("随机日期生成失败! 失败原因: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
         if (varname != null) {
             JMeterVariables vars = getVariables();
             if (vars != null && varname.length() > 0) {
@@ -64,17 +68,13 @@ public class WZRandomBirth extends AbstractFunction {
     public void setParameters(Collection<CompoundVariable> collection) throws InvalidVariableException {
         checkParameterCount(collection, 1, 4);
         Object[] values = collection.toArray();
-        if (values.length > 0) {
-            strStart = ((CompoundVariable) values[0]).execute().trim();
-            if (strStart.length() < 1) {
-                System.out.println("start date must not be empty");
-                System.exit(2);
-            }
-        }
+        dateS = (values.length > 0)
+                ? ((CompoundVariable) values[0]).execute().trim()
+                : null;
         if (values.length > 1) {
-            strEnd = ((CompoundVariable) values[1]).execute().trim();
-            if (strEnd.length() < 1) {
-                strEnd = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+            dateE = ((CompoundVariable) values[1]).execute().trim();
+            if (dateE.length() < 1) {
+                dateE = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
             }
         }
         if (values.length > 2) {
@@ -83,9 +83,9 @@ public class WZRandomBirth extends AbstractFunction {
                 format = "yyyy-MM-dd";
             }
         }
-        if (values.length > 3) {
-            varname = ((CompoundVariable) values[3]).execute().trim();
-        }
+        varname = (values.length > 3)
+                ? ((CompoundVariable) values[3]).execute().trim()
+                : null;
     }
 
     public String getReferenceKey() {
